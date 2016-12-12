7e1121cca07c62dd1cac1a545125eb933c3711c2
Skye Freeman <skyefreeman@icloud.com>
2016-12-12 13:29:03 -0500

    updated config

---
+++ web-bloglovin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+open https://github.com/bloglovin/ios
+open https://github.com/bloglovin/airtower/pulls
+open https://bloglovin.atlassian.net/secure/RapidBoard.jspa?rapidView=26&projectKey=IOSN

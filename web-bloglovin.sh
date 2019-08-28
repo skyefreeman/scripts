113deb32a0f57d34f197138cfa6ada77a36556f7
skyefreeman <skyefreeman@icloud.com>
2019-08-28 16:12:05 -0700

    Deleted old scripts that haven't been used in years

--- web-bloglovin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-open https://bloglovin.atlassian.net/secure/RapidBoard.jspa?rapidView=26&projectKey=IOSN
-open https://apidoc.devlovin.com
-open https://github.com/bloglovin/ios
-open https://github.com/bloglovin/airtower
-open https://github.com/bloglovin/fuselage
-open https://github.com/bloglovin/android
